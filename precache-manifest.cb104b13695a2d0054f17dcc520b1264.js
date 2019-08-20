2623c49fd9c688f44e4d1ebeffba6d3b9a86a3e7
Maurice Christopher <maurice@opus1.io>
2019-08-19 17:07:37 -0700

    Updates

---
+++ precache-manifest.cb104b13695a2d0054f17dcc520b1264.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f36c80dcc518bdc2b711",
+    "url": "/movie-list/static/js/runtime~main.de49e91d.js"
+  },
+  {
+    "revision": "dfd7d88f6bb1b7e9e154",
+    "url": "/movie-list/static/js/main.95e2f7d6.chunk.js"
+  },
+  {
+    "revision": "a5c473f373653ee6bb58",
+    "url": "/movie-list/static/js/2.c585ec42.chunk.js"
+  },
+  {
+    "revision": "dfd7d88f6bb1b7e9e154",
+    "url": "/movie-list/static/css/main.b16e205c.chunk.css"
+  },
+  {
+    "revision": "bc64136135182c358e280bca935a6219",
+    "url": "/movie-list/index.html"
+  }
+];
